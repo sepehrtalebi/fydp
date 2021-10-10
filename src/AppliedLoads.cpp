3f770b913cfa0f4e539e9c8d8bbae18bc9731aa4
Amaar Quadri <amaarquadri@gmail.com>
2021-10-10 19:30:37 -0400

    Update AppliedLoads.cpp

--- src/AppliedLoads.cpp
+++ src/AppliedLoads.cpp
@@ -1,6 +1,6 @@
 #include "AppliedLoads.h"
 
-Wrench<double> getPropellerLoads(const Vector<double, KF::n> &state, const ControlInputs &control_inputs) {
+static Wrench<double> getPropellerLoads(const Vector<double, KF::n> &state, const ControlInputs &control_inputs) {
     auto propeller_voltage = control_inputs.PropellerVoltage;
     if (propeller_voltage > 12) propeller_voltage = 12; //saturation
     else if (propeller_voltage < 0) propeller_voltage = 0; //saturation
