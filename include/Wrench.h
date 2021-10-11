0984f158d2f4d2aa78791ac532ed33173529b854
Amaar Quadri <amaarquadri@gmail.com>
2021-10-10 20:06:14 -0400

    add operators to Wrench.h, explicitly create Vector3

--- include/Wrench.h
+++ include/Wrench.h
@@ -8,8 +8,38 @@ struct Wrench {
     Vector3<T> torque;
 
     Wrench<T> operator+(const Wrench<T> &other) {
-        return Wrench<T>{force + other.force, torque + other.torque};
+        return {Vector3<T>{force + other.force}, Vector3<T>{torque + other.torque}};
     }
 
-    // TODO: add other operators
+    Wrench<T> operator-(const Wrench<T> &other) {
+        return {Vector3<T>{force - other.force}, Vector3<T>{torque - other.torque}};
+    }
+
+    Wrench<T> operator*(const T &scalar) {
+        return {Vector3<T>{force * scalar}, Vector3<T>{torque * scalar}};
+    }
+
+    Wrench<T> operator/(const double &scalar) {
+        return {Vector3<T>{force / scalar}, Vector3<T>{torque / scalar}};
+    }
+
+    void operator+=(const Wrench<T> &other) {
+        force += other.force;
+        torque += other.torque;
+    }
+
+    void operator-=(const Wrench<T> &other) {
+        force -= other.force;
+        torque -= other.torque;
+    }
+
+    void operator*=(const T &scalar) {
+        force *= scalar;
+        torque *= scalar;
+    }
+
+    void operator/=(const double &scalar) {
+        force /= scalar;
+        torque /= scalar;
+    }
 };
