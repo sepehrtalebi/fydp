10aa8908ad591ee413e5381559328d27c0daf644
Amaar Quadri <amaarquadri@gmail.com>
2021-10-24 15:50:06 -0400

    implement rail detection and optical flow models

--- include/Constants.h
+++ include/Constants.h
@@ -6,30 +6,40 @@
 // environmental constants
 static constexpr const double GRAVITATIONAL_ACCELERATION = 9.81; // m/s^2
 static constexpr const double ATMOSPHERIC_PRESSURE = 101325; // Pa
-static constexpr const double AIR_DENSITY = 1.225; //kg/m^3
+static constexpr const double AIR_DENSITY = 1.225; // kg/m^3
+
+// rail track constants
+static constexpr const double RAIL_WIDTH = 1.4351; // m
+static const Vector3<double> NORTH{1, 0, 0}; // NOLINT(cert-err58-cpp)
+
+// camera constants
+static constexpr const double CAMERA_GAIN = 1;
+static constexpr const int HALF_CAMERA_HORIZONTAL_PIXELS = 1920 / 2;
+static constexpr const int HALF_CAMERA_VERTICAL_PIXELS = 1080 / 2;
+static constexpr const double OPTICAL_FLOW_VELOCITY_GAIN = 1.3;
 
 // aircraft inertial constants
 static constexpr const double MASS = 1; // aircraft mass, kg
-static const Vector3<double> WEIGHT{0, 0, MASS * GRAVITATIONAL_ACCELERATION};
-static const Matrix<double, 3, 3> INERTIA_TENSOR{1, 0, 0, 0, 1, 0, 0, 0, 1};
-static const Matrix<double, 3, 3> INERTIA_TENSOR_INV = INERTIA_TENSOR.inv();
+static const Vector3<double> WEIGHT{0, 0, MASS * GRAVITATIONAL_ACCELERATION}; // NOLINT(cert-err58-cpp)
+static const Matrix<double, 3, 3> INERTIA_TENSOR{1, 0, 0, 0, 1, 0, 0, 0, 1}; // NOLINT(cert-err58-cpp)
+static const Matrix<double, 3, 3> INERTIA_TENSOR_INV = INERTIA_TENSOR.inv(); // NOLINT(cert-err58-cpp)
 
 // aileron aerodynamic constant
 static constexpr const double LIFT_GAIN_AILERON = 1;
 // displacement of aerodynamic center of ailerons from the center of mass
-static const Vector3<double> L_RIGHT_AILERON{0, 1, 0};
-static const Vector3<double> L_LEFT_AILERON{L_RIGHT_AILERON[0], -L_RIGHT_AILERON[1], L_RIGHT_AILERON[2]};
+static const Vector3<double> L_RIGHT_AILERON{0, 1, 0}; // NOLINT(cert-err58-cpp)
+static const Vector3<double> L_LEFT_AILERON{L_RIGHT_AILERON[0], -L_RIGHT_AILERON[1], L_RIGHT_AILERON[2]}; // NOLINT(cert-err58-cpp)
 
 // aircraft body aerodynamic constants
 static constexpr const double DRAG_GAIN_BODY = 1;
 static constexpr const double LIFT_GAIN_BODY = 1;
 // displacement of aerodynamic center of the body from the center of mass
-static const Vector3<double> L_BODY{0, 0, 0};
+static const Vector3<double> L_BODY{0, 0, 0}; // NOLINT(cert-err58-cpp)
 
 // rudder aerodynamic constant
 static constexpr const double LIFT_GAIN_RUDDER = 1;
 // displacement of aerodynamic center of rudder from the center of mass
-static const Vector3<double> L_RUDDER{-1, 0, 0};
+static const Vector3<double> L_RUDDER{-1, 0, 0}; // NOLINT(cert-err58-cpp)
 
 static constexpr const double T_SAMPLE = 1E-3; //sampling period
 
@@ -38,11 +48,11 @@ static constexpr const double K_PROPELLER = 1;
 static constexpr const double TAU_PROPELLER = 1;
 static constexpr const double THRUST_GAIN_PROPELLER = 1;
 static constexpr const double TORQUE_GAIN_PROPELLER = 1;
-static const Vector3<double> L_FRONT_PROPELLER{0, 0, 0};
+static const Vector3<double> L_FRONT_PROPELLER{0, 0, 0}; // NOLINT(cert-err58-cpp)
 
 //elevator constants
 static constexpr const double LIFT_GAIN_ELEVATOR = 1;
-static const Vector3<double> L_ELEVATOR{0, 0, 0};
+static const Vector3<double> L_ELEVATOR{0, 0, 0}; // NOLINT(cert-err58-cpp)
 
 // Kalman filter constants
 constexpr static const int n = 31; // number of states
--- src/SensorModels.cpp
+++ src/SensorModels.cpp
@@ -1,12 +1,57 @@
 #include "SensorModels.h"
+#include "Vector3.h"
+#include "Quaternion.h"
+#include "KF.h"
+#include <cmath>
 
-static void railDetection(const Vector<double, n> &state, SensorMeasurements &sensor_measurements) {
-    sensor_measurements.found_rail = false;
+static void railDetection(const Quaternion<double> &quat, const double &altitude, SensorMeasurements &sensor_measurements) {
+    if (altitude < 0) {
+        // cannot see rail if we are underground
+        // leave found_rail as false and leave rail_pixel_x, rail_pixel_y, rail_pixel_width, and rail_angle as 0
+        return;
+    }
+
+    Vector3<double> down_earth = quat.unrotate({0, 0, 1}); // down (i.e. the direction the camera is pointing) in earth coordinates
+    if (down_earth.z < 0) {
+        // cannot see rail if the plane is upside down
+        return;
+    }
+
+    // rail offset in body coordinates
+    Vector3<double> rail_offset_body = quat.rotate(down_earth * altitude / down_earth.z);
+
+    Vector3<double> rail_pixel_location = rail_offset_body * CAMERA_GAIN / rail_offset_body.z;
+    if (HALF_CAMERA_HORIZONTAL_PIXELS < std::abs(rail_pixel_location.x) ||
+            HALF_CAMERA_VERTICAL_PIXELS < std::abs(rail_pixel_location.y)) {
+        // rail tracks are out of the field of view of the camera
+        return;
+    }
+
+    // rail tracks are visible
+    sensor_measurements.found_rail = true;
+    sensor_measurements.rail_pixel_x = rail_pixel_location.x;
+    sensor_measurements.rail_pixel_y = rail_pixel_location.y;
+    sensor_measurements.rail_pixel_width = RAIL_WIDTH * CAMERA_GAIN / rail_offset_body.z;
+
+    Vector3<double> rail_direction_body = quat.rotate(NORTH); // rail direction in body coordinates
+    Vector3<double> rail_direction_pixels = rail_direction_body * CAMERA_GAIN / rail_offset_body.z;
+    sensor_measurements.rail_angle = std::atan2(rail_direction_pixels.y, rail_direction_pixels.x);
+}
+
+static void opticalFlow(const Vector3<double> &velocity, const double &altitude, SensorMeasurements &sensor_measurements) {
+    Vector3<double> pixel_velocity = velocity* OPTICAL_FLOW_VELOCITY_GAIN / altitude;
+    sensor_measurements.pixel_velocity[0] = pixel_velocity.x;
+    sensor_measurements.pixel_velocity[1] = pixel_velocity.y;
 }
 
 SensorMeasurements getSensorMeasurements(const Vector<double, n> &state) {
+    Quaternion<double> quat{state[KF::q0], state[KF::q1], state[KF::q2], state[KF::q3]};
+    Vector3<double> velocity{state[KF::vx], state[KF::vy], state[KF::vz]};
+    double altitude = -state[KF::pz];
+
     SensorMeasurements sensor_measurements;
-    railDetection(state, sensor_measurements);
+    railDetection(quat, altitude, sensor_measurements);
+    opticalFlow(velocity, altitude, sensor_measurements);
 
 //    Wrench<double> wrench = applied_loads.getAppliedLoads(x);
 //    // TODO
