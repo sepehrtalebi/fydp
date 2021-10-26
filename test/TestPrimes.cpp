d231af0d551d7d8bfe3b3619137b1e3e9fbdf4dc
Amaar Quadri <aq@trexorobotics.com>
2021-10-26 11:44:22 -0400

    added test for prime factorization of 2

--- test/TestPrimes.cpp
+++ test/TestPrimes.cpp
@@ -4,17 +4,21 @@
 #include <iostream>
 
 void testPrimes() {
-    std::vector<int> factors = primeFactorization(69); // 3 * 23
-    assert(factors.size() == 2);
-    assert(factors[0] == 3);
-    assert(factors[1] == 23);
+    std::vector<int> factors69 = primeFactorization(69); // 3 * 23
+    assert(factors69.size() == 2);
+    assert(factors69[0] == 3);
+    assert(factors69[1] == 23);
 
-    std::vector<int> factors2 = primeFactorization(140); // 2 * 2 * 5 * 7
-    assert(factors2.size() == 4);
+    std::vector<int> factors140 = primeFactorization(140); // 2 * 2 * 5 * 7
+    assert(factors140.size() == 4);
+    assert(factors140[0] == 2);
+    assert(factors140[1] == 2);
+    assert(factors140[2] == 5);
+    assert(factors140[3] == 7);
+
+    std::vector<int> factors2 = primeFactorization(2); // 2
+    assert(factors2.size() == 1);
     assert(factors2[0] == 2);
-    assert(factors2[1] == 2);
-    assert(factors2[2] == 5);
-    assert(factors2[3] == 7);
 
     std::cout << "Passed All Tests for primes!" << std::endl;
 }
