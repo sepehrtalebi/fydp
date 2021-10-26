5844c3b7d8ec67023cee06267dff0fe0c5a4ff98
Amaar Quadri <aq@trexorobotics.com>
2021-10-26 11:49:08 -0400

    improve fourier testing, optionally print benchmarks with macro

--- test/TestFourier.cpp
+++ test/TestFourier.cpp
@@ -6,26 +6,50 @@
 #include <cassert>
 #include <iostream>
 
-#define TEST_SIZE 100
+#define BENCHMARK 0
 
-void testFourier() {
+#if BENCHMARK == 1
+#include <chrono>
+#endif
+
+void testFFT(const int &N) {
     std::vector<std::complex<double>> test1;
     std::vector<std::complex<double>> test2;
-    test1.reserve(TEST_SIZE);
-    test2.reserve(TEST_SIZE);
-    for (int i = 0; i < TEST_SIZE; i++) {
+    test1.reserve(N);
+    test2.reserve(N);
+    for (int i = 0; i < N; i++) {
         test1.emplace_back(1, 1);
         test2.emplace_back(1, 1);
     }
-
+#if BENCHMARK == 1
+    auto t1 = std::chrono::high_resolution_clock::now();
     Fourier<double>::fft(test1.begin(), test1.end());
-    Fourier<double>::dft(test2.begin(), test2.end(), 1, Fourier<double>::getRootsOfUnity(TEST_SIZE));
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> fft_ms = t2 - t1;
 
-    for (int i = 0; i < TEST_SIZE; i++) {
+    t1 = std::chrono::high_resolution_clock::now();
+    Fourier<double>::dft(test2.begin(), 1, Fourier<double>::getRootsOfUnity(N), N);
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> dft_ms = t2 - t1;
+
+    std::cout << "FFT is " << dft_ms.count() / fft_ms.count() << " times faster for N = " << N << std::endl;
+#else
+    Fourier<double>::fft(test1.begin(), test1.end());
+    Fourier<double>::dft(test2.begin(), 1, Fourier<double>::getRootsOfUnity(N), N);
+#endif
+
+    for (int i = 0; i < N; i++) {
         std::complex<double> error = test1[i] - test2[i];
-        assert(std::abs(error.real()) < 0.01);
-        assert(std::abs(error.imag()) < 0.01);
+        assert(std::abs(error.real()) < 0.0001);
+        assert(std::abs(error.imag()) < 0.0001);
     }
+}
+
+void testFourier() {
+    for (int i = 2; i < 1000; i++) {
+        testFFT(i);
+    }
+    testFFT(10000);
 
     std::cout << "Passed All Tests for Fourier!" << std::endl;
 }
