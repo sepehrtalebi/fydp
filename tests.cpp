71e58a6dcea8e17da5860b5d1113e544688a9b24
corwinmacmillan <cgjmacmi@edu.uwaterloo.ca>
2021-11-28 19:52:12 -0500

    Polynomial test (outdated)

--- tests.cpp
+++ tests.cpp
@@ -5,16 +5,18 @@
 #include "test/TestDifferentiation.h"
 #include "test/TestPrimes.h"
 #include "test/TestFourier.h"
+#include "controller/TestPolynomial.h"
 #include <iostream>
 
 int main() {
-    testVector();
-    testVector3();
-    testQuaternion();
-    testMatrix();
-    testDifferentiation();
-    testPrimes();
-    testFourier();
-    std::cout << "Passed All Tests!" << std::endl;
+//    testVector();
+//    testVector3();
+//    testQuaternion();
+//    testMatrix();
+//    testDifferentiation();
+//    testPrimes();
+//    testFourier();
+//    std::cout << "Passed All Tests!" << std::endl;
+    testPolynomial();
     return 0;
 }
