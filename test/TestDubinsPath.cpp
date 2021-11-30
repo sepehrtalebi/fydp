c97707165b028ca15d822714fe1fe55d46dd1a01
Amaar Quadri <amaarquadri@gmail.com>
2021-11-30 15:54:07 -0500

    added DubinsPath tests

---
+++ test/TestDubinsPath.cpp
@@ -0,0 +1,14 @@
+#include "TestDubinsPath.h"
+#include "DubinsPath.h"
+#include <iostream>
+
+void testDubinsPath() {
+    using Vector2 = Vector<double, 2>;
+    State<double> start = {Vector2{0, 0}, Vector2{0, 1}};
+    State<double> goal = {Vector2{10, 0}, Vector2{0, -1}};
+    double radius = 1;
+    DubinsPath<double> path = DubinsPath<double>::create(start, goal, radius);
+    std::cout << path.toStr() << std::endl;
+
+    std::cout << "Passed all tests for DubinsPath!" << std::endl;
+}
