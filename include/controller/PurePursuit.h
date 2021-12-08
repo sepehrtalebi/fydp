35b5f0b3c46a2299ad27f6382836112c94f83ad4
Amaar Quadri <amaarquadri@gmail.com>
2021-12-07 23:50:05 -0500

    implement pure pursuit class

--- include/controller/PurePursuit.h
+++ include/controller/PurePursuit.h
@@ -10,13 +10,35 @@ public:
         T angular_vel;
     };
 
+    PurePursuit(const T& lookahead_time) : lookahead_time(lookahead_time) {}
+
+    void updatePath(const DubinsPath<T>& new_path) {
+        this->path = new_path;
+    }
+
     /**
      *
-     * @param path
      * @param state
      * @return Commanded angular velocity
      */
-    Result pursue(const DubinsPath<T>& path, const typename DubinsPath<T>::State& state) {
+    Result pursue(const typename DubinsPath<T>::State& state) {
+        Vector2 ideal_target = state.pos + state.vel * lookahead_time;
+        Vector2 target = path.closestPoint(ideal_target);
+        // TODO: add check to see if we are too far off course
 
+        // readjust to a coordinate system where we are at the origin and moving in the direction of the x-axis
+        target -= state.pos;
+        target = getRotationMatrix(std::atan2(state.vel[1], state.vel[0])).transpose() * target;
+
+        // now the center of rotation must be at the point of intersection of the y-axis and
+        // the perpendicular bisector of the line segment connecting target to the origin
+        // simple geometry shows that the y coordinate where this occurs is given by the following
+        T radius = target.magnitudeSquared() / (2 * target[1]);
+        return {false, state.vel.magnitude() / radius};
     }
+
+private:
+    using Vector2 = Vector<T, 2>;
+    T lookahead_time;
+    DubinsPath<T> path;
 };
