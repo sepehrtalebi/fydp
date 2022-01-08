247e44911f65f1e9b5688f0ad22cbecb4bfd8581
Amaar Quadri <amaarquadri@gmail.com>
2022-01-08 18:34:04 -0500

    implement compile time constants for integers

--- include/compiled_differentiation/CConstant.h
+++ include/compiled_differentiation/CConstant.h
@@ -30,4 +30,27 @@ namespace compiled {
 
     template<typename T>
     inline constexpr bool is_constant_v = is_constant<T>::value;
+
+    template<char... cs>
+    struct parse_ratio;
+
+    template<char... cs>
+    inline constexpr int parse_ratio_v = parse_ratio<cs...>::value;
+
+    template<char c>
+    struct parse_ratio<c> {
+      static constexpr size_t power = 1;
+      static constexpr int value = c - '0';
+    };
+
+    template<char c, char... cs>
+    struct parse_ratio<c, cs...> {
+      static constexpr size_t power = 10 * parse_ratio<cs...>::power;
+      static constexpr int value = (c - '0') * power + parse_ratio_v<cs...>;
+    };
+
+    template<char... cs>
+    constexpr auto operator "" _c() {
+      return Constant<std::ratio<parse_ratio_v<cs...>>>{};
+    }
 }
--- test/TestCompiledDifferentiation.cpp
+++ test/TestCompiledDifferentiation.cpp
@@ -15,15 +15,10 @@ void testOperators() {
 
     auto x = Variable<0>{};
     auto y = Variable<1>{};
-    auto two = Constant<std::ratio<2>>{};
-    auto three = Constant<std::ratio<3>>{};
-    auto four = Constant<std::ratio<4>>{};
-    auto six = Constant<std::ratio<6>>{};
-    auto seven = Constant<std::ratio<7>>{};
 
-    auto t = getHigherDerivative<1>(x, three * x + x);
+    auto t = getHigherDerivative<1>(x, 3_c * x + x);
 
-    ASSERT_EQUAL(four, getDerivative(x, four * x + y));
+    ASSERT_EQUAL(4_c, getDerivative(x, 4_c * x + y));
 }
 
 void testCompiledDifferentiation() {
@@ -31,33 +26,27 @@ void testCompiledDifferentiation() {
 
     auto x = Variable<0>{};
     auto y = Variable<1>{};
-    auto two = Constant<std::ratio<2>>{};
-    auto three = Constant<std::ratio<3>>{};
-    auto four = Constant<std::ratio<4>>{};
-    auto five = Constant<std::ratio<45>>{};
-    auto six = Constant<std::ratio<6>>{};
-    auto seven = Constant<std::ratio<7>>{};
 
-    static_assert(is_constant_v<decltype(three)>);
+    static_assert(is_constant_v<decltype(3_c)>);
     static_assert(is_constant_v<Zero>);
     static_assert(is_constant_v<One>);
     static_assert(!is_constant_v<decltype(x)>);
-    static_assert(is_constant_v<decltype(three + four)>);
+    static_assert(is_constant_v<decltype(3_c + 4_c)>);
 
-    ASSERT_EQUAL(zero, zero);
-    ASSERT_EQUAL(seven, three + four);
-    ASSERT_EQUAL(three, three + zero);
-    static_assert(getNodeCount(three + zero) == 1);
-    static_assert(getNodeCount(three + two) == 1);
+    ASSERT_EQUAL(0_c, 0_c);
+    ASSERT_EQUAL(7_c, 3_c + 4_c);
+    ASSERT_EQUAL(3_c, 3_c + 0_c);
+    static_assert(getNodeCount(3_c + 0_c) == 1);
+    static_assert(getNodeCount(3_c + 2_c) == 1);
     static_assert(getNodeCount(x + y) == 3);
     ASSERT_EQUAL(one, getDerivative(x, x + y));
     ASSERT_EQUAL(x, getHigherDerivative<0>(x, x));
     ASSERT_EQUAL(one, getDerivative(x, x));
     ASSERT_EQUAL(one, getHigherDerivative<1>(x, x));
     static_assert(getNodeCount(getDerivative(x, x + y)) == 1);
-    ASSERT_EQUAL(x * two, getDerivative(x, pow(x, two)));
-    ASSERT_EQUAL(two * x - four, x - four + x);
+    ASSERT_EQUAL(x * 2_c, getDerivative(x, pow(x, 2_c)));
+    ASSERT_EQUAL(2_c * x - 4_c, x - 4_c + x);
 
-//    static_assert(std::is_same_v<product_t<two, x>, deep_simplify_t<derivative_t<0, power_t<x, two>>>>);
-//    static_assert(std::is_same_v<product_t<six, x>, higher_derivative_t<0, 2, power_t<x, three>>>);
+//    static_assert(std::is_same_v<product_t<2_c, x>, deep_simplify_t<derivative_t<0, power_t<x, 2_c>>>>);
+//    static_assert(std::is_same_v<product_t<6_c, x>, higher_derivative_t<0, 2, power_t<x, 3_c>>>);
 }
