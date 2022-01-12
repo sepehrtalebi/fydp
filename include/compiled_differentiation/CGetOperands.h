144e72ca124fe5829d4ab1164dfbd82ff4547388
Amaar Quadri <amaarquadri@gmail.com>
2022-01-12 14:31:09 -0500

    added get_left_operand and get_right_operand

---
+++ include/compiled_differentiation/CGetOperands.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "CDifference.h"
+#include "CProduct.h"
+#include "CSum.h"
+#include <type_traits>
+
+namespace compiled {
+
+/**
+ * Returns the L in sum<L, R>, difference<L, R>, or product<L, R>.
+ * If the given type is not an instance of any of these, then void is returned.
+ */
+template <typename T>
+struct get_left_operand {
+  using type = void;
+};
+
+template <typename T>
+using get_left_operand_t = typename get_left_operand<T>::type;
+
+template <typename L, typename R>
+struct get_left_operand<sum<L, R>> {
+  using type = L;
+};
+
+template <typename L, typename R>
+struct get_left_operand<difference<L, R>> {
+  using type = L;
+};
+
+template <typename L, typename R>
+struct get_left_operand<product<L, R>> {
+  using type = L;
+};
+
+/**
+ * Returns the R in sum<L, R>, difference<L, R>, or product<L, R>.
+ * If the given type is not an instance of any of these, then void is returned.
+ */
+template <typename T>
+struct get_right_operand {
+  using type = void;
+};
+
+template <typename T>
+using get_right_operand_t = typename get_right_operand<T>::type;
+
+template <typename L, typename R>
+struct get_right_operand<sum<L, R>> {
+  using type = R;
+};
+
+template <typename L, typename R>
+struct get_right_operand<difference<L, R>> {
+  using type = R;
+};
+
+template <typename L, typename R>
+struct get_right_operand<product<L, R>> {
+  using type = R;
+};
+
+}  // namespace compiled
