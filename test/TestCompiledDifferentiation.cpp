d26391ffd3914ab3b20390e65f4924a5408675b6
Amaar Quadri <amaarquadri@gmail.com>
2022-01-12 16:16:29 -0500

    define function for getting demangled class names

--- test/TestCompiledDifferentiation.cpp
+++ test/TestCompiledDifferentiation.cpp
@@ -1,12 +1,32 @@
 #include "TestCompiledDifferentiation.h"
-
 #include "CConstant.h"
-#include "CVariable.h"
+#include "CDeepSimplify.h"
 #include "CDerivative.h"
 #include "CHigherDerivative.h"
-#include "CDeepSimplify.h"
+#include "CVariable.h"
 
+#include <array>
+#include <iostream>
+#include <string>
 #include <type_traits>
+
+#ifdef __GNUG__
+#include <cxxabi.h>
+#include <memory>
+#endif
+
+template <typename T>
+std::string get_class_name(const T& t) {
+  const char* name = typeid(t).name();
+#ifdef __GNUG__
+  int status;
+  std::unique_ptr<char, void (*)(void*)> res{
+      abi::__cxa_demangle(name, NULL, NULL, &status), std::free};
+  return (status == 0) ? res.get() : name;
+#else
+  return name;
+#endif
+}
 
 #define ASSERT_EQUAL(T1, T2) static_assert(std::is_same_v<decltype(T1), decltype(T2)>)
 
