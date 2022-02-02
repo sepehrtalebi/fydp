572c4e8e72b5f2b059406da00a5955cef314ca44
Amaar Quadri <amaarquadri@gmail.com>
2022-02-02 02:30:16 -0500

    move main function to control loop cpp file

--- bmb_state_estimation/src/StateEstimationControlLoop.cpp
+++ bmb_state_estimation/src/StateEstimationControlLoop.cpp
@@ -1,9 +1,22 @@
 #include "bmb_state_estimation/StateEstimationControlLoop.h"
 
+#include <ros/ros.h>
+#include <bmb_state_estimation/UKF.h>
+
+
 StateEstimationControlLoop::StateEstimationControlLoop() {
   ros::Subscriber sub = nh.subscribe("sensor_measurements", 1, sensor);
 }
 
 StateEstimationControlLoop::spin() {
 
+}
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "bmb_state_estimation");
+  ros::NodeHandle nh;
+  UKF kf{};
+  StateEstimationControlLoop control_loop{nh, kf};
+  control_loop.spin();
 }
