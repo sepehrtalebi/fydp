a50ff53c1930bd2d26fd3aefa3f231bad574cc28
Amaar Quadri <amaarquadri@gmail.com>
2022-02-02 13:47:14 -0500

    add subscribers and publishers

--- bmb_state_estimation/src/StateEstimationControlLoop.cpp
+++ bmb_state_estimation/src/StateEstimationControlLoop.cpp
@@ -2,10 +2,44 @@
 
 #include <ros/ros.h>
 #include <bmb_state_estimation/UKF.h>
-
+#include <bmb_msgs/AircraftState.h>
 
 StateEstimationControlLoop::StateEstimationControlLoop() {
-  ros::Subscriber sub = nh.subscribe("sensor_measurements", 1, sensor);
+  // initialize subscribers
+  pressure_sensor_sub_ = nh.subscribe(
+      "pressure_sensor_reading", 1, &StateEstimationControlLoop::pressureSensorCallback, this);
+  imu_sub_ = nh.subscribe(
+      "imu_reading", 1, &StateEstimationControlLoop::imuCallback, this);
+  gps_sub_ = nh.subscribe(
+      "gps_reading", 1, &StateEstimationControlLoop::gpsCallback, this);
+  rail_detection_sub_ = nh.subscribe(
+      "rail_detection", 1, &StateEstimationControlLoop::railDetectionCallback, this);
+  optical_flow_sub_ = nh.subscribe(
+      "optical_flow_reading", 1, &StateEstimationControlLoop::opticalFlowCallback, this);
+
+  // initialize publishers
+  aircraft_state_pub_ = nh.advertise<bmb_msgs::AircraftState>("aircraft_state", 1);
+}
+
+
+void StateEstimationControlLoop::pressureSensorCallback(const sensor_msgs::FluidPressure& msg) {
+
+}
+
+void StateEstimationControlLoop::imuCallback(const sensor_msgs::Imu& msg) {
+
+}
+
+void StateEstimationControlLoop::gpsCallback(const sensor_msgs::NavSatFix& msg) {
+
+}
+
+void StateEstimationControlLoop::railDetectionCallback(const bmb_msgs::RailDetection& msg) {
+
+}
+
+void StateEstimationControlLoop::opticalFlowCallback(const bmb_msgs::OpticalFlow& msg) {
+
 }
 
 StateEstimationControlLoop::spin() {
