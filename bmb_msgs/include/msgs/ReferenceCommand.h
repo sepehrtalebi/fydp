8b89f6f08f7fa2b5c5d396de11193a5fb56ef9ce
Amaar Quadri <amaarquadri@gmail.com>
2022-02-02 04:22:16 -0500

    delete deprecated files

--- bmb_msgs/include/msgs/ReferenceCommand.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <bmb_math/Vector.h>
-
-struct ReferenceCommand {
-    double airspeed;
-    double altitude;
-    Vector<double, 2> waypoint;
-};
--- bmb_msgs/src/ControlInputs.cpp
+++
@@ -1,9 +0,0 @@
-#include <bmb_msgs/ControlInputs.h>
-
-ControlInputs ControlInputs::parseU(const Vector<double, 4> &inputs) {
-    return {inputs[0], inputs[1], inputs[2], inputs[3]};
-}
-
-Vector<double, 4> ControlInputs::getU() const {
-    return Vector<double, 4>{propeller_voltage, right_aileron_angle, left_aileron_angle, elevator_angle};
-}
