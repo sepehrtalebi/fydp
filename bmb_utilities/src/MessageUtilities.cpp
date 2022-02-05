e68c05fbd00423b9002a7de2c3ea8b824d13d600
Amaar Quadri <amaarquadri@gmail.com>
2022-02-05 02:29:19 -0500

    fix typos

--- bmb_utilities/src/MessageUtilities.cpp
+++ bmb_utilities/src/MessageUtilities.cpp
@@ -37,27 +37,27 @@ Vector<double, bmb_msgs::SensorMeasurements::SIZE> as_vector(
 bmb_msgs::SensorMeasurements as_msg(
     const Vector<double, bmb_msgs::SensorMeasurements::SIZE>& sensor_measurements) {
   bmb_msgs::SensorMeasurements msg;
-  msg.pressure_reading.fluid_pressure = sensor_meausrements[bmb_msgs::SensorMeasurements::PRESSURE];
-  msg.rail_detection.angle = sensor_meausrements[bmb_msgs::SensorMeasurements::RAIL_ANGLE];
-  msg.rail_detection.pixel_width = sensor_meausrements[bmb_msgs::SensorMeasurements::RAIL_WIDTH];
-  msg.rail_detection.pixel_x = sensor_meausrements[bmb_msgs::SensorMeasurements::RAIL_x];
-  msg.rail_detection.pixel_y = sensor_meausrements[bmb_msgs::SensorMeasurements::RAIL_y];
-  msg.rail_detection.found = sensor_meausrements[bmb_msgs::SensorMeasurements::RAIL_FOUND];
-  msg.optical_flow_reading.x_pixel_velocity = sensor_meausrements[bmb_msgs::SensorMeasurements::PIXEL_vx];
-  msg.optical_flow_reading.y_pixel_velocity = sensor_meausrements[bmb_msgs::SensorMeasurements::PIXEL_vy];
-  msg.imu_reading.orientation.w = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_q0];
-  msg.imu_reading.orientation.x = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_q1];
-  msg.imu_reading.orientation.y = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_q2];
-  msg.imu_reading.orientation.z = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_q3];
-  msg.imu_reading.angular_velocity.x = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_wx];
-  msg.imu_reading.angular_velocity.y = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_wy];
-  msg.imu_reading.angular_velocity.z = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_wz];
-  msg.imu_reading.linear_acceleration.x = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_ax];
-  msg.imu_reading.linear_acceleration.y = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_ay];
-  msg.imu_reading.linear_acceleration.z = sensor_meausrements[bmb_msgs::SensorMeasurements::IMU_az];
-  msg.gps_reading.latitude = sensor_meausrements[bmb_msgs::SensorMeasurements::LAT];
-  msg.gps_reading.longitude = sensor_meausrements[bmb_msgs::SensorMeasurements::LONG];
-  msg.gps_reading.altitude = sensor_meausrements[bmb_msgs::SensorMeasurements::ALT];
+  msg.pressure_reading.fluid_pressure = sensor_measurements[bmb_msgs::SensorMeasurements::PRESSURE];
+  msg.rail_detection.angle = sensor_measurements[bmb_msgs::SensorMeasurements::RAIL_ANGLE];
+  msg.rail_detection.pixel_width = sensor_measurements[bmb_msgs::SensorMeasurements::RAIL_WIDTH];
+  msg.rail_detection.pixel_x = sensor_measurements[bmb_msgs::SensorMeasurements::RAIL_x];
+  msg.rail_detection.pixel_y = sensor_measurements[bmb_msgs::SensorMeasurements::RAIL_y];
+  msg.rail_detection.found = sensor_measurements[bmb_msgs::SensorMeasurements::RAIL_FOUND];
+  msg.optical_flow_reading.x_pixel_velocity = sensor_measurements[bmb_msgs::SensorMeasurements::PIXEL_vx];
+  msg.optical_flow_reading.y_pixel_velocity = sensor_measurements[bmb_msgs::SensorMeasurements::PIXEL_vy];
+  msg.imu_reading.orientation.w = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_q0];
+  msg.imu_reading.orientation.x = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_q1];
+  msg.imu_reading.orientation.y = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_q2];
+  msg.imu_reading.orientation.z = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_q3];
+  msg.imu_reading.angular_velocity.x = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_wx];
+  msg.imu_reading.angular_velocity.y = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_wy];
+  msg.imu_reading.angular_velocity.z = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_wz];
+  msg.imu_reading.linear_acceleration.x = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_ax];
+  msg.imu_reading.linear_acceleration.y = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_ay];
+  msg.imu_reading.linear_acceleration.z = sensor_measurements[bmb_msgs::SensorMeasurements::IMU_az];
+  msg.gps_reading.latitude = sensor_measurements[bmb_msgs::SensorMeasurements::LAT];
+  msg.gps_reading.longitude = sensor_measurements[bmb_msgs::SensorMeasurements::LONG];
+  msg.gps_reading.altitude = sensor_measurements[bmb_msgs::SensorMeasurements::ALT];
   return msg;
 }
 
@@ -80,7 +80,7 @@ Vector<double, bmb_msgs::AircraftState::SIZE> as_vector(const bmb_msgs::Aircraft
 }
 
 bmb_msgs::AircraftState as_msg(const Vector<double, bmb_msgs::AircraftState::SIZE>& aircraft_state) {
-  bmb::msgs::AircraftState msg;
+  bmb_msgs::AircraftState msg;
   msg.pose.position.x = aircraft_state[bmb_msgs::AircraftState::PX];
   msg.pose.position.y = aircraft_state[bmb_msgs::AircraftState::PY];
   msg.pose.position.z = aircraft_state[bmb_msgs::AircraftState::PZ];
@@ -99,7 +99,7 @@ bmb_msgs::AircraftState as_msg(const Vector<double, bmb_msgs::AircraftState::SIZ
 
 Vector3<double> as_vector(const bmb_msgs::ControlInputs& msg) {
   Vector3<double> vec;
-  vec[bmb_msgs::ControlInputs::PROPELLER] = msg.propellor_voltage;
+  vec[bmb_msgs::ControlInputs::PROPELLER] = msg.propeller_voltage;
   vec[bmb_msgs::ControlInputs::RIGHT_AILERON] = msg.right_aileron_angle;
   vec[bmb_msgs::ControlInputs::ELEVATOR] = msg.elevator_angle;
   return vec;
@@ -107,9 +107,9 @@ Vector3<double> as_vector(const bmb_msgs::ControlInputs& msg) {
 
 bmb_msgs::ControlInputs as_msg(const Vector3<double>& control_inputs) {
   bmb_msgs::ControlInputs msg;
-  msg.propellor_voltage = vec[bmb_msgs::ControlInputs::PROPELLER];
-  msg.right_aileron_angle = vec[bmb_msgs::ControlInputs::RIGHT_AILERON];
-  msg.elevator_angle = vec[bmb_msgs::ControlInputs::ELEVATOR];
+  msg.propeller_voltage = control_inputs[bmb_msgs::ControlInputs::PROPELLER];
+  msg.right_aileron_angle = control_inputs[bmb_msgs::ControlInputs::RIGHT_AILERON];
+  msg.elevator_angle = control_inputs[bmb_msgs::ControlInputs::ELEVATOR];
   return msg;
 }
 
