8e12f33da7636f855350e255f883c8f03ea229db
Amaar Quadri <amaarquadri@gmail.com>
2022-02-05 18:59:27 -0500

    add ConstexprUtils.h

---
+++ bmb_utilities/include/bmb_utilities/ConstexprUtils.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+
+namespace bmb_utilities {
+
+/**
+ * Unrolls a loop over the specified values.
+ * The passed in function should be a lambda with a single argument "auto i"
+ * where i is the loop variable which is constexpr.
+ */
+template <int start, int stop, int step = 1, class F>
+constexpr void constexprFor(F&& f)
+{
+  static_assert(step != 0);
+
+  if constexpr (step > 0 && start < stop || step < 0 && start > stop)
+  {
+    f(std::integral_constant<size_t , start>());
+    constexprFor<start + step, stop, step>(f);
+  }
+}
+
+} // namespace bmb_utilities
