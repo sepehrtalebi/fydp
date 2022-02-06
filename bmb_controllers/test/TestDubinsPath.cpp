c4a90279a97ca4a1ad2f424d22c96e6cdac19d09
Amaar Quadri <amaarquadri@gmail.com>
2022-02-05 20:03:14 -0500

    update tests in bmb_controllers to use gtest

--- bmb_controllers/test/TestDubinsPath.cpp
+++ bmb_controllers/test/TestDubinsPath.cpp
@@ -1,38 +1,41 @@
-#include "TestDubinsPath.h"
 #include <bmb_controllers/DubinsPath.h>
 
-#include <cassert>
-#include <cmath>
+#include <gtest/gtest.h>
+#include <ros/package.h>
 #include <fstream>
-#include <iostream>
 
-void testDubinsPath() {
+TEST(TestDubinsPath, testDubinsPath) {
+    const std::string path = ros::package::getPath("bmb_controllers") + "/test/output/";
     using Vector2 = Vector<double, 2>;
     DubinsPath<double>::State start = {Vector2{0, 0}, Vector2{0, 1}};
     DubinsPath<double>::State goal = {Vector2{10, 0}, Vector2{0, -1}};
-    double radius = 1;
+    static constexpr double radius = 1;
     DubinsPath<double> path = DubinsPath<double>::create(start, goal, radius);
-    assert(path[0].is_turning && path[0].isRightTurn() && !path[1].is_turning && path[2].is_turning && path[2].isRightTurn());
-    assert(std::fabs((path.closestPoint(Vector2{4.5, 1}) - Vector2{4.5, 1}).magnitude()) < 0.001);
+    ASSERT_TRUE(path[0].is_turning && path[0].isRightTurn() &&
+                !path[1].is_turning && path[2].is_turning && path[2].isRightTurn());
+    ASSERT_TRUE(std::fabs((path.closestPoint(Vector2{4.5, 1}) - Vector2{4.5, 1}).magnitude()) < 0.001);
 
-    std::ofstream out1("test/output/dubins_path1.csv");
-    assert(out1);
+    std::ofstream out1(path + "dubins_path1.csv");
+    ASSERT_TRUE(out1);
     path.toCSV(out1);
     out1.close();
 
     goal = {Vector2{5, 3}, Vector2{-1, 2}};
     path = DubinsPath<double>::create(start, goal, radius);
-    std::ofstream out2("test/output/dubins_path2.csv");
-    assert(out2);
+    std::ofstream out2(path + "dubins_path2.csv");
+    ASSERT_TRUE(out2);
     path.toCSV(out2);
     out2.close();
 
     goal = {Vector2{0.2, 1.1}, Vector2{-0.2, -3}};
     path = DubinsPath<double>::create(start, goal, radius);
-    std::ofstream out3("test/output/dubins_path3.csv");
-    assert(out3);
+    std::ofstream out3(path + "dubins_path3.csv");
+    ASSERT_TRUE(out3);
     path.toCSV(out3);
     out3.close();
+}
 
-    std::cout << "Passed all tests for DubinsPath!\n";
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- bmb_controllers/test/TestPID.cpp
+++ bmb_controllers/test/TestPID.cpp
@@ -1,6 +1,12 @@
+#include <bmb_math/TransferFunction.h>
+
 #include <gtest/gtest.h>
 
 TEST(TestPID, testPID) {
     TransferFunction<double, 1, 3> {1, 1, 2, 3};
+}
 
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
