420c659f039841c4cf0d63b3987b769635e9c768
Amaar Quadri <amaarquadri@gmail.com>
2022-02-07 15:37:13 -0500

    clean up GlobalPathPlannerNode

---
+++ bmb_controllers/include/bmb_controllers/GlobalPathPlannerNode.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <bmb_msgs/AircraftState.h>
+#include <bmb_msgs/ReferenceCommand.h>
+#include <ros/ros.h>
+#include <cstddef>
+#include <fstream>
+#include <optional>
+#include <vector>
+
+class GlobalPathPlannerNode {
+ public:
+  GlobalPathPlannerNode();
+
+  void spin();
+
+  // TODO: starting point in global coordinates has to be stored in memory (in
+  //  GPS Sensor, accessible by service),
+  //  then global path planner needs to convert relative coordinate to
+  //  global coordinate
+ private:
+  void aircraftStateCallback(const bmb_msgs::AircraftState& msg);
+
+  static constexpr double RADIUS_TOL = 10;  // m
+
+  ros::Subscriber aircraft_state_sub_;
+  ros::Publisher reference_command_pub_;
+  std::vector<bmb_msgs::ReferenceCommand> reference_commands;
+  size_t waypoint_index;
+};
