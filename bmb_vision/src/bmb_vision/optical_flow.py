41bf8a14239cf58b3069c558faaa053f64099db2
Amaar Quadri <amaarquadri@gmail.com>
2022-02-08 03:51:38 -0500

    fix python output message type

--- bmb_vision/src/bmb_vision/optical_flow.py
+++ bmb_vision/src/bmb_vision/optical_flow.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import Image
 class OpticalFlow:
     def __init__(self):
         self.image_sub = rospy.Subscriber("/rectified_image", Image, self.image_callback)
-        self.optical_flow_pub = rospy.Publisher("/optical_flow_reading", Image, queue_size=1)
+        self.optical_flow_pub = rospy.Publisher("/optical_flow_reading", OpticalFlowReading, queue_size=1)
 
     def image_callback(self, image):
         # TODO: compute optical_flow_reading based on reading
