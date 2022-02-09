fbfdf5834b9efc3e61c6a6b30e0107a6a1d18e44
Amaar Quadri <amaarquadri@gmail.com>
2022-02-09 01:27:16 -0500

    bug fix

--- bmb_vision/nodes/rail_detection_node.py
+++ bmb_vision/nodes/rail_detection_node.py
@@ -5,7 +5,7 @@ from bmb_vision.rail_detection_helper import RailDetectionHelper
 
 
 def main():
-    rospy.init_node("/rail_detection_node")
+    rospy.init_node("rail_detection_node")
     rail_detection_helper = RailDetectionHelper()
     rail_detection_helper.spin()
 
