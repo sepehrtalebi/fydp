662e4b03ab1736c77d00baeb980dec9fdece45fb
Amaar Quadri <amaarquadri@gmail.com>
2022-02-09 01:33:09 -0500

    update default filter coefficient

--- bmb_utilities/include/bmb_utilities/ControllerGains.h
+++ bmb_utilities/include/bmb_utilities/ControllerGains.h
@@ -6,6 +6,6 @@ struct ControllerGains {
 
   ControllerGains(const double& kp = 0, const double& ki = 0,
                   const double& kd = 0, const double& kff = 0,
-                  const double& N = 0)
+                  const double& N = 100)
       : kp(kp), ki(ki), kd(kd), kff(kff), N(N) {}
 };
