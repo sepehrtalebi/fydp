bd1822c05d4996eb61fa4b4c85e6ea2bbc662384
Amaar Quadri <amaarquadri@gmail.com>
2022-02-10 03:08:46 -0500

    merge LocalPathPlanner into a single header/cpp, clean up

--- bmb_controllers/include/bmb_controllers/LocalPathPlannerNode.h
+++ bmb_controllers/include/bmb_controllers/LocalPathPlannerNode.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <bmb_controllers/DubinsPath.h>
+#include <bmb_controllers/PIDFFController.h>
+#include <bmb_controllers/PurePursuit.h>
 #include <bmb_msgs/AircraftState.h>
 #include <bmb_msgs/ReferenceCommand.h>
 #include <bmb_msgs/StateCommand.h>
@@ -16,7 +19,18 @@ class LocalPathPlannerNode {
 
   void referenceCommandCallback(const bmb_msgs::ReferenceCommand& msg);
 
+  bmb_msgs::StateCommand getStateCommand(const bmb_msgs::AircraftState& state);
+
+  DubinsPath<double> path;
+  bool update_dubins_path{false};
+  PurePursuit<double> pursuer;
+  PIDFFController<double> altitude_pid;
+
   double update_frequency;
+  bmb_msgs::AircraftState latest_aircraft_state;
+  bmb_msgs::ReferenceCommand latest_reference_command;
+
+  // subscribers and publishers
   ros::Subscriber aircraft_state_sub_;
   ros::Subscriber reference_command_sub_;
   ros::Publisher state_command_pub_;
