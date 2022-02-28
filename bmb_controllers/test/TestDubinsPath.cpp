eaa45968299856b5096124808bd343e727902e20
Amaar Quadri <amaarquadri@gmail.com>
2022-02-28 03:30:40 -0500

    fix class names

--- bmb_controllers/test/TestDubinsPath.cpp
+++ bmb_controllers/test/TestDubinsPath.cpp
@@ -1,3 +1,4 @@
+#include <bmb_controllers/PosVelState.h>
 #include <bmb_controllers/DubinsPath.h>
 #include <bmb_math/Vector.h>
 #include <gtest/gtest.h>
@@ -5,12 +6,11 @@
 #include <fstream>
 
 TEST(TestDubinsPath, testDubinsPath) {
-  using State = DubinsPath<double>::State;
   const std::string directory =
       ros::package::getPath("bmb_controllers") + "/test/output/";
   using Vector2 = Vector<double, 2>;
-  State start{Vector2{0, 0}, Vector2{0, 1}};
-  State goal{Vector2{10, 0}, Vector2{0, -1}};
+  PosVelState start{Vector2{0, 0}, Vector2{0, 1}};
+  PosVelState goal{Vector2{10, 0}, Vector2{0, -1}};
   static constexpr double radius = 1;
   DubinsPath<double> path = DubinsPath<double>::create(start, goal, radius);
   ASSERT_TRUE(path[0].is_turning && path[0].isRightTurn() &&
@@ -24,7 +24,7 @@ TEST(TestDubinsPath, testDubinsPath) {
   path.toCSV(out1);
   out1.close();
 
-  goal = State{Vector2{5, 3}, Vector2{-1, 2}};
+  goal = PosVelState{Vector2{5, 3}, Vector2{-1, 2}};
   path = DubinsPath<double>::create(start, goal, radius);
   std::ofstream out2(directory + "dubins_path2.csv");
   ASSERT_TRUE(out2);
