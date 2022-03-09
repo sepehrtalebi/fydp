affaab8109879b327c55b78ec8f11fc4df006eb3
Amaar Quadri <amaarquadri@gmail.com>
2022-03-09 16:58:18 -0500

    clean up optical flow

--- bmb_vision/src/bmb_vision/optical_flow.py
+++ bmb_vision/src/bmb_vision/optical_flow.py
@@ -7,35 +7,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from time import perf_counter
 
-#global vars
-prvs = 0
 
 class OpticalFlow:
     def __init__(self):
         self.image_sub = rospy.Subscriber("/rectified_image", Image, self.image_callback)
         self.optical_flow_pub = rospy.Publisher("/optical_flow_reading", OpticalFlowReading, queue_size=1)
+        self.prvs = None
 
     def image_callback(self, image):
-        
-        #converting ROS message to opencv image
+        # converting ROS message to opencv image
         frame2 = bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
-        next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-        
-        if prvs!=0:          
-            flow = cv2.calcOpticalFlowFarneback(prvs, next, None,0.5, 3, 15, 3, 5, 1.2, 0)
-            if 1==2: #np.var(flow) > ? ---- still determining best way to do variance formula. Will update. 
-                velocity = None
-            else:    
-                velocity = np.mean(flow, axis = (0,1))
-            prvs = next
+        next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+        if self.prvs is not None:
+            flow = cv2.calcOpticalFlowFarneback(self.prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            if False:  # np.var(flow) > ? ---- TODO: still determining best way to do variance formula. Will update.
+                velocity = (0, 0)
+            else:
+                velocity = np.mean(flow, axis=(0, 1))
         else:
-            velocity = (0,0)
-            
-        optical_flow_reading = velocity
+            velocity = (0, 0)
 
+        self.prvs = next
+
+        optical_flow_reading.x_pixel_velocity = velocity[0]
+        optical_flow_reading.y_pixel_velocity = velocity[1]
         self.optical_flow_pub.publish(optical_flow_reading)
-        
-        
 
-    def spin(self):
+    @staticmethod
+    def spin():
         rospy.spin()
